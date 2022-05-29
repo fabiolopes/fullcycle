ba79ca1388e861ee9cd7b77aaffad8c9391d7dfc
fabiolopes <fabiolopesbione@hotmail.com>
2022-05-29 15:52:25 -0300

    desafio golang

--- my_push.sh
+++ my_push.sh
@@ -2,13 +2,16 @@
 
 branch=main
 message="dummy commit" 
+toCommit="."
 
 if [ $# -ge 2 ]
   then
     branch=$1
     message=$2
+    replace=" "
+    toCommit=`echo $3 | sed -e "s/,/$replace/g"`
 fi
 
-git add .
+git add ${toCommit:=.}
 git commit -m $message
 git push origin $branch 
