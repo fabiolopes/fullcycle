43c696bc7caaf95a02addf73640a396c5c1dd1ca
fabiolopes <fabiolopesbione@hotmail.com>
2022-06-03 00:29:33 -0300

    desafio_2 pronto

---
+++ mod_docker/desafios/desafio2_node_nginx/node/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+const mysql = require('mysql')
+
+function executeQuery(sql) {
+    const connection = mysql.createConnection(config)
+    connection.query(sql)
+    connection.end()
+}
+
+app.get('/', (req, res) => {
+    executeQuery(`insert into people(name) values('Bione')`)
+    var connection = mysql.createConnection(config)
+    connection.query("select name from people", function(err, result, fields){
+        if (err) throw err;
+        res.send('<h1>Full Cycle</h1><br>' + JSON.stringify(result));
+    });
+})
+
+app.listen(port, ()=>{
+    console.log('Rodando na porta ' + port)
+})
